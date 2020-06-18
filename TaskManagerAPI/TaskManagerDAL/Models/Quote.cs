3ec7816121abe74eb6181bffbe01a818133f5410
nasi7 <55255943+nasi7@users.noreply.github.com>
2020-06-18 12:36:45 -0500

    edits

---
+++ TaskManagerAPI/TaskManagerDAL/Models/Quote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TaskManagerDAL.Models
+{
+    class Quote
+    {
+        public int QuoteID { get; set; }
+        public string QuoteType { get; set; }
+        public string Contact { get; set; }
+        public string TaskDescription { get; set; }
+        public DateTime DueDate { get; set; }
+        public string TaskType { get; set; }
+    }
+}
